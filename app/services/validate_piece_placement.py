467fbb10f209e0c097f729fb24d2a2b8dee7bae1
Justin Mills <justin@softwheresolutions.com>
2025-05-08 20:03:30 -0500

    Resolving issues with running outside of virtual environment and with validate piece logic.

--- app/services/validate_piece_placement.py
+++ app/services/validate_piece_placement.py
@@ -1,7 +1,9 @@
 def valid_piece_placement(board):
-    # Validate that no 1 or 2 is placed above a 0 in the board
-    for row in range(len(board) - 1):  # Exclude the last row, because it has no piece below it
-        for col in range(len(board[row])):
-            if board[row][col] != 0 and board[row + 1][col] == 0:
-                return False, "Invalid piece placement. Blue or Red should not exist on top of an empty space."  # Invalid placement
-    return True
+    for col in range(len(board[0])):
+        empty_found = False
+        for row in range(len(board)):
+            if board[row][col] == 0:
+                empty_found = True
+            elif empty_found and board[row][col] != 0:
+                return False, f"Invalid piece at ({row}, {col}): placed on top of an empty slot"
+    return True, ""
